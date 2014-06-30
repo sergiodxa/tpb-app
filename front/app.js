f93641ae04c05e8e57d3f30d3fc45ba700bee1f3
Sergio Daniel Xalambrí <sergio@xalambri.com.ar>
2014-06-30 20:31:25 -0300

    frontend and backend almost ready

--- front/app.js
+++ front/app.js
@@ -3,10 +3,29 @@
 *
 * WebApp in NodeJS to search torrents in The Pirate Bay
 */
-angular.module('TPBApp', ['ngRoute']).config(function ($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'views/home.html'
-    })
-    .otherwise({ redirectTo: '/' });
-})
+angular.module('TPBApp', ['ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        controller : HomeCtrl,
+        templateUrl: 'views/home.html'
+      })
+      .otherwise({ redirectTo: '/' });
+  });
+
+function HomeCtrl ($http, $scope) {
+  $scope.search = function () {
+    console.log('ok');
+    $http({
+      method: 'GET',
+      url   : '/search',
+      params: {
+        query : $scope.query
+      }
+    }).success(function (data) {
+      console.log(data);
+    }).error(function (data) {
+      console.log(data);
+    });
+  };
+};
--- server.js
+++ server.js
@@ -1,16 +1,33 @@
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+var express = require('express');
+var logfmt  = require('logfmt');
+var tpb     = require('thepiratebay');
+var app     = express();
 
 app.use(logfmt.requestLogger());
 
 app.use(express.static(__dirname + '/front'));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendfile(__dirname + '/front/index.html');
+});
+
+app.get('/search', function (req, res) {
+  var query = req.query.query;
+
+  tpb.search(query, {
+    category: 'all',
+    page    : 0,
+    orderBy : 'seeds desc'
+  }, function (err, res) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(res);
+    }
+  })
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log('Listening on ' + port);
 });
