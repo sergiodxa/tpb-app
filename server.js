097f2ef63d59e02937709368e46b8622f43ff473
Sergio Daniel Xalambrí <sergio@xalambri.com.ar>
2014-06-30 21:21:23 -0300

    console.log added to see the response of tpb

--- server.js
+++ server.js
@@ -20,8 +20,10 @@ app.get('/search', function (req, res) {
     orderBy : 'seeds desc'
   }, function (err, res) {
     if (err) {
+      console.log(err);
       res.send(false);
     } else {
+      console.log(res);
       res.send(res);
     }
   })
