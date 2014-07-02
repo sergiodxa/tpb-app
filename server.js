742009bcaf34e710372b1b9fb894518a8d81a017
Sergio Daniel Xalambrí <sergio@xalambri.com.ar>
2014-07-01 22:32:32 -0300

    return orderBy to be hardcoded

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/search', function (req, res) {
   // call method search of tpb with the params searchQuery, all categories, order descendant by seeds and if the query is successful send the results to the response, else send false.
   tpb.search(searchQuery, {
       category: 'all',
-      orderBy : orderByQuery
+      orderBy : 'seeds desc'
   }).then(function (results){
       res.send(results);
   }).catch(function (err){
